26c201d1758e0865e9acb9759e78a2068c83f5eb
Devin Mitchell <devin.a.mitchell@gmail.com>
2025-01-17 11:46:52 -0700

    Add utils

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const NAV_SECTIONS: { title: string, path: string }[] = [
+  {
+    title: 'Code',
+    path: '/code'
+  },
+  {
+    title: 'About',
+    path: '/about'
+  },
+  {
+    title: 'Blog',
+    path: '/blog'
+  },
+  {
+    title: 'Film',
+    path: '/film'
+  },
+  {
+    title: 'Lettering',
+    path: '/lettering'
+  },
+]
---
+++ src/utils/fonts.ts
@@ -0,0 +1,4 @@
+import { Figtree, IBM_Plex_Mono } from "next/font/google";
+
+export const figTree = Figtree({ subsets: ["latin"] });
+export const ibmPlexMono = IBM_Plex_Mono({ weight: ['100', '200', '300', '400', '500', '600', '700'], subsets: ['latin'] })
