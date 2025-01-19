da62cfecf00eb72462d35b4906b1a19b0da6df13
Devin Mitchell <devin.a.mitchell@gmail.com>
2025-01-18 19:07:00 -0700

    Make hero-post most recent post and add small mobil nav animation

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -6,7 +6,7 @@ export function Intro() {
       <h1 className=" animate-fadeInUp text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <h4 className={`${ibmPlexMono.className} animate-fadeInUp text-center md:text-left text-lg mt-5 md:pl-8`}>
+      <h4 className={`animate-fadeInUp text-center md:text-left text-lg mt-5 md:pl-8`}>
         A roughly curated collection of thoughts & ideas, snapshots of my life, and other things I think are worth sharing. {" "}
       </h4>
     </section>
--- src/app/_components/mobile-nav.tsx
+++ src/app/_components/mobile-nav.tsx
@@ -10,9 +10,9 @@ export default function MobileNav() {
   return (
     <>
       <button onClick={() => setIsOpen(true)} className="w-8 h-8 md:hidden flex flex-col justify-around items-center">
-        <div className="h-1 w-8 rounded-full bg-zinc-900" />
-        <div className="h-1 w-8 rounded-full bg-zinc-900" />
-        <div className="h-1 w-8 rounded-full bg-zinc-900" />
+        <div className={`${isOpen ? '-translate-x-24 opacity-0' : ''} transition ease-in-out  opacity-1  h-1 w-8  rounded-full bg-zinc-900`} />
+        <div className={`${isOpen ? '-translate-x-24 opacity-0' : ''} transition ease-in-out delay-150 opacity-1  h-1 w-8 rounded-full bg-zinc-900`} />
+        <div className={`${isOpen ? '-translate-x-24 opacity-0' : ''} transition ease-in-out delay-300 opacity-1 h-1 w-8 rounded-full bg-zinc-900`} />
       </button>
       <div className={`${isOpen ? 'flex' : 'animate-fadeOutDown'} absolute top-0 bottom-0 left-0 right-0 animate-fadeInUp bg-blue-500 flex flex-col justify-center`}>
         <button onClick={() => setIsOpen(false)} className="absolute top-8 right-8 flex items-center justify-center border border-zinc-900 p-2 rounded-full  h-12 w-12 flex active:bg-blue-300">
