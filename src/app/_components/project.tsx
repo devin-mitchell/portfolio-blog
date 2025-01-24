f7cb15d226f660a8a295537eff9d7e7ae8a3ee37
Devin Mitchell <devin.a.mitchell@gmail.com>
2025-01-24 11:31:02 -0700

    Adding new styles for Code section

--- src/app/_components/project.tsx
+++ src/app/_components/project.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
-import { ProjectType } from "../code/page"
 import { figTree, ibmPlexMono } from "./fonts"
+import { ProjectType } from "@/utils/constants"
 
 
 export function Project({ title, description, links, technologies }: ProjectType) {
   return (
-    <div className="h-full w-full flex flex-col justify-between bg-zinc-300 rounded-xl px-4 py-4">
+    <div className="h-full w-full flex flex-col justify-between rounded-xl px-4 py-4 bg-gradient-to-br from-violet-300 from-20% via-fuchsia-700 via-30% to-rose-900 via-70%">
       <h2 className={`${figTree.className} text-4xl lg:text-6xl text-center font-bold mb-8`}>{title}</h2>
       <p className="mb-8">{description}</p>
       <div className="flex flex-wrap gap-2 mb-4">
@@ -17,9 +17,9 @@ export function Project({ title, description, links, technologies }: ProjectType
           })
         }
       </div>
-      <div className="flex justify-end text-2xl gap-4 underline">
-        {links?.github && <Link href={links.github}>GitHub</Link>}
-        {links?.deployment && <a href={links.deployment}>Deployed Site</a>}
+      <div className="flex justify-end text-2xl gap-4 underline text-white">
+        {links?.github && <a target="_blank" href={links.github}>GitHub</a>}
+        {links?.deployment && <a target="_blank" href={links.deployment}>Deployed Site</a>}
       </div>
     </div>
   )
--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -8,7 +8,7 @@ export default function Index() {
     <main>
       <Container>
         <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-          <h1 className=" animate-fadeInUp text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          <h1 className="animate-fadeInUp text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
             CODE
           </h1>
           <h4 className="animate-fadeInUp text-center md:text-left text-lg mt-5 md:pl-8">
@@ -26,7 +26,7 @@ export default function Index() {
                 key={project.title}
                 initial={{ opacity: 0, translateX: fadeDirection }}
                 whileInView={{ opacity: 1, translateX: 0 }}
-                className="w-full transition duration-200 ease-linear "
+                className="w-full rounded-lg duration-200 ease-linear"
               >
                 <Project {...project} />
               </motion.div>
