97422951f8562163abadc798845ce4ef621282c9
Devin Mitchell <devin.a.mitchell@gmail.com>
2025-02-07 13:01:21 -0700

    Change hero post

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,12 +3,15 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'CODE',
+}
 
 export default function Index() {
   const allPosts = getAllPosts();
-
-  const heroPost = allPosts[3];
-
+  const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
 
   return (
