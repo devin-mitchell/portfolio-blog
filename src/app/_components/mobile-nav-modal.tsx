612aeeb2f78511f73ec9048a08db598ca3d873f0
Devin Mitchell <devin.a.mitchell@gmail.com>
2025-02-12 10:52:54 -0700

    Fix mobile nav spacing for small devices

--- src/app/_components/mobile-nav-modal.tsx
+++ src/app/_components/mobile-nav-modal.tsx
@@ -20,13 +20,13 @@ export function MobileNavModal() {
           className={`no-doc-scroll z-10 absolute top-0 h-screen w-screen  flex flex-col justify-center bg-gradient-to-br from-violet-300 from-20% via-fuchsia-300 via-30% to-rose-300 via-70% overflow-y-hidden`}>
           <button
             onClick={() => remove()}
-            className="absolute top-8 right-8 flex items-center justify-center border border-zinc-900 p-2 rounded-full  h-12 w-12 flex active:bg-zinc-50">
+            className="absolute top-8 right-8 flex items-center justify-center border border-zinc-900 p-2 rounded-full p-3 h-12 w-12 flex active:bg-zinc-50">
             <ExitIcon />
           </button>
-          <div className="flex flex-col mb-12 h-1/2 w-full items-center">
+          <div className="flex flex-col w-full items-center">
             {NAV_SECTIONS.map(({ title, path }, i) => {
               return (
-                <Link key={path} href={path} onClick={() => remove()} className={`${i != 0 ? 'mt-8' : ''} text-6xl w-full  text-center active:bg-zinc-50`}>{title}</Link>
+                <Link key={path} href={path} onClick={() => remove()} className={`${i != 0 ? 'mt-8' : ''} text-5xl w-full  text-center active:bg-zinc-50`}>{title}</Link>
               )
             })}
           </div>
